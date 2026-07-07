901d7793278c7e7128603dff9082a1a981cc708f
gogo <gogo@local>
2026-07-07 23:27:43 +0800

    remove sidebar category icons

--- web/src/App.vue
+++ web/src/App.vue
@@ -23,7 +23,6 @@
         <div class="sb-label">分类</div>
         <div v-for="t in types" :key="t.name" class="sb-item"
           :class="{on: curType===t.name}" @click="pick(t.name)">
-          <span v-html="categoryIconSvg(t.icon, t.name)"></span>
           <span>{{ t.name || '未分类' }}</span>
         </div>
       </nav>
@@ -134,7 +133,6 @@ import { applySiteHead, applySiteTheme, readCachedSite, themePageFromRoute, writ
 import { openAuthDialog } from './authDialog'
 import { currentUser, refreshUser } from './userStore'
 import { levelTagStyle } from './levelTag'
-import { categoryIconSvg } from './categoryIcons'
 import { applyPwaUpdate, setupPwaUpdates } from './pwa'
 
 const router = useRouter()
